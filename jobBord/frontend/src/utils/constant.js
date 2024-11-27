4b8275d75c0dd32565c5bea5caa3322ed86cf5d8
adarsh kumar singh <adarsh@kali>
2024-11-27 22:11:54 +0530

    base url change of jobBord

--- jobBord/frontend/src/utils/constant.js
+++ jobBord/frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BaseUrl = "http://localhost:8000";
+const BaseUrl = "https://codsoft-w5ct.onrender.com";
 export const USER_API_END_POINT = `${BaseUrl}/api/v1/user`;
 export const JOB_API_END_POINT = `${BaseUrl}/api/v1/job`;
 export const APPLICATION_API_END_POINT = `${BaseUrl}/api/v1/application`;
